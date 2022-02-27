acbbeac71a8f08bc3f5599d2cdf7cd9930d743c8
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-02-27 23:38:28 +0530

    shell script for init setup

---
+++ init_setup.sh
@@ -0,0 +1,21 @@
+echo [$(date)]: "START"
+echo [$(date)]: "creating environment"
+conda create --prefix ./env python=3.7 -y
+echo [$(date)]: "activate environment"
+source activate ./env
+echo [$(date)]: "install requirements"
+pip install -r requirements.txt
+echo [$(date)]: "export conda environment"
+conda env export > conda.yaml
+echo [$(date)]: "initialize git repository"
+git init
+echo [$(date)]: "add env to gitignore"
+echo "env/" > .gitignore
+# echo "# ${PWD}" > README.md
+echo [$(date)]: "first commit"
+git add .
+git commit -m "first commit"
+echo [$(date)]: "END"
+
+# to remove everything -
+# rm -rf env/ .gitignore conda.yaml README.md .git/
