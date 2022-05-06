31ba694bd33bc7c7709d75f6cbf573270c034fa5
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-05-07 01:26:59 +0530

    removed git init

--- init_setup.sh
+++ init_setup.sh
@@ -7,14 +7,6 @@ echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "export conda environment"
 conda env export > conda.yaml
-echo [$(date)]: "initialize git repository"
-git init
-echo [$(date)]: "add env to gitignore"
-echo "env/" > .gitignore
-# echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
 echo [$(date)]: "END"
 
 # to remove everything -
